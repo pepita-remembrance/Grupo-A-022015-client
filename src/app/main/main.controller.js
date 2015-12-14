3db75d1ed6003e058b16b9b111122ae6514b8bec
ariel <alvarez.ariel.work@gmail.com>
2015-12-14 16:53:05 -0300

    Initial commit

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,37 @@
+export class MainController {
+  constructor ($timeout, webDevTec, toastr, Restangular) {
+    'ngInject';
+
+    this.awesomeThings = [];
+    this.classAnimation = '';
+    this.creationDate = 1450048660366;
+    this.toastr = toastr;
+
+    Restangular.all('players').getList()
+      .then((players)=> {
+        this.players = players;
+      });
+
+    this.activate($timeout, webDevTec);
+  }
+
+  activate($timeout, webDevTec) {
+    this.getWebDevTec(webDevTec);
+    $timeout(() => {
+      this.classAnimation = 'rubberBand';
+    }, 4000);
+  }
+
+  getWebDevTec(webDevTec) {
+    this.awesomeThings = webDevTec.getTec();
+
+    angular.forEach(this.awesomeThings, (awesomeThing) => {
+      awesomeThing.rank = Math.random();
+    });
+  }
+
+  showToastr() {
+    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+    this.classAnimation = '';
+  }
+}
