28faa3f6292f87504fec75701ed59930064841fb
Federico Aloi <federico.aloi@gmail.com>
2015-12-15 18:44:52 -0300

    More i18n

--- src/app/index.i18n.js
+++ src/app/index.i18n.js
@@ -3,20 +3,34 @@ export function i18nConfig ($translateProvider) {
   $translateProvider
 
   .translations('en', {
+    'DEFENDER': 'DEFENDER',
+    'FORWARD': 'FORWARD',
+    'GOALKEEPER': 'GOALKEEPER',
+    'MIDFIELDER': 'MIDFIELDER',
     'Leagues': 'Leagues',
     'Logout': 'Logout',
     'Name': 'Name',
     'Open': 'Open',
     'Players': 'Players',
+    'Save': 'Save',
+    'Score': 'Score',
+    'Scores': 'Scores',
     'Teams': 'Teams'
   })
 
   .translations('es', {
+    'DEFENDER': 'DEFENSOR',
+    'FORWARD': 'DELANTERO',
+    'GOALKEEPER': 'ARQUERO',
+    'MIDFIELDER': 'MEDIOCAMPISTA',
     'Leagues': 'Ligas',
     'Logout': 'Cerrar sesion',
     'Name': 'Nombre',
     'Open': 'Abrir',
     'Players': 'Jugadores',
+    'Save': 'Guardar',
+    'Score': 'Puntaje',
+    'Scores': 'Puntajes',
     'Teams': 'Equipos'
   })
 
