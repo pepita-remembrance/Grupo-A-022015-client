4f5375b76c5f0362a12fddb064ce96781f2a74d6
Federico Aloi <federico.aloi@gmail.com>
2015-12-15 18:49:49 -0300

    More i18n

--- src/app/index.i18n.js
+++ src/app/index.i18n.js
@@ -9,12 +9,16 @@ export function i18nConfig ($translateProvider) {
     'MIDFIELDER': 'MIDFIELDER',
     'Leagues': 'Leagues',
     'Logout': 'Logout',
+    'Min Teams': 'Min Teams',
+    'Max Teams': 'Max Teams',
     'Name': 'Name',
     'Open': 'Open',
     'Players': 'Players',
     'Save': 'Save',
     'Score': 'Score',
     'Scores': 'Scores',
+    'Stage': 'Stage',
+    'Stages': 'Stages',
     'Teams': 'Teams'
   })
 
@@ -25,12 +29,16 @@ export function i18nConfig ($translateProvider) {
     'MIDFIELDER': 'MEDIOCAMPISTA',
     'Leagues': 'Ligas',
     'Logout': 'Cerrar sesion',
+    'Min Teams': 'Minimo de equipos',
+    'Max Teams': 'Maximo de equipos',
     'Name': 'Nombre',
     'Open': 'Abrir',
     'Players': 'Jugadores',
     'Save': 'Guardar',
     'Score': 'Puntaje',
     'Scores': 'Puntajes',
+    'Stage': 'Fecha',
+    'Stages': 'Fechas',
     'Teams': 'Equipos'
   })
 
