ad33ecf3dbe18efcd543eb46b72b75db0bf79af7
Federico Aloi <federico.aloi@gmail.com>
2015-12-15 14:34:01 -0300

    Auth errors login via console

--- src/app/auth/login.controller.js
+++ src/app/auth/login.controller.js
@@ -1,7 +1,7 @@
 import { CoreController }    from '../core/core.controller';
 
 export class LoginController {
-  constructor ($scope, auth, store, $location) {
+  constructor ($scope, auth, store, $location, $log) {
     'ngInject';
 
     auth.signin({}, (profile, token) => {
@@ -9,7 +9,9 @@ export class LoginController {
       store.set('profile', profile);
       store.set('token', token);
       $location.path('/');
-    }, () => {});
+    }, (error) => {
+      $log.error(error);
+    });
   }
 
 }
