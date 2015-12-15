7fdc200242554a94b781ccc76ec97c50d2534a04
Federico Aloi <federico.aloi@gmail.com>
2015-12-15 15:50:53 -0300

    Added interceptor to authenticate requests

--- src/app/index.auth.js
+++ src/app/index.auth.js
@@ -1,8 +1,14 @@
-export function authConfig (authProvider) {
+export function authConfig (authProvider, $httpProvider, jwtInterceptorProvider) {
   'ngInject';
   authProvider.init({
     domain: 'pepita.auth0.com',
     clientID: 'uYjeLTz1Jg7JFuNzmdCKvRZndOBErxPW',
     loginState: 'login'
   });
+
+  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+    return store.get('token');
+  }];
+
+  $httpProvider.interceptors.push('jwtInterceptor');
 }
